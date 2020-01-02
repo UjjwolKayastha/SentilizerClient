6b9d29047aabc8d9a08030ad962ce6e523d6b838
Ujjwol Kayastha <uzol123@gmail.com>
2020-01-02 12:04:17 +0545

    Added Tableau Dashboard from Tableau Public

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import QueryForm from './components/queryform';
+import Display from './components/display';
+import Display2 from './components/display2';
+import Display3 from './components/display3';
+import Display4 from './components/display4';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +11,14 @@ class App extends Component {
     return (
       <div className="App">
         <QueryForm />
+        <Display />
+        <Display2/>
+        <Display3/>
+        <Display4/>
       </div>
+
+      
+                
     );
   }
 }
---
+++ src/components/display2.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import tableau from 'tableau-api';
+
+class Display extends Component {
+    initTableau(){
+        const vizUrl = "https://public.tableau.com/views/TESLATWEETSENTIMENTANALYSIS/Dashboard22?:display_count=y&:origin=viz_share_link&:embed=true";
+        //options
+        const options = {
+            hideTabs:true,
+            width:"1366px",
+            height:"768px",
+            onFirstInteractive:()=>{
+                const sheet = viz.getWorkbook().getActiveSheet().getWorksheets().get("Table");
+                const options = {
+                    ignoreAliases: false,
+                    ignoreSelection: false,
+                    includeAllColumns: false
+                };
+            }
+    }
+    let viz = new window.tableau.Viz(this.container, vizUrl, options);
+        this.setState({
+            viz:viz
+        })
+}
+constructor(props) {
+    super(props);
+    this.state  = {viz:{}};
+}
+    render() {
+        return (
+            <div ref={c => (this.container = c)}
+            style={{position:'fit'}}
+            >
+                
+            </div>
+        )
+    }
+    componentDidMount(){
+        this.initTableau();
+    }
+}
+
+
+export default Display;
